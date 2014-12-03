2a94d5ccfb66d7551b202fe69f97873288cb0735
Bo Zhu <github@bozhu.me>
2014-12-02 23:15:36 -0500

    basically work; add sentry; TODO: analytics, tests

--- main.py
+++ main.py
@@ -4,31 +4,38 @@ import os
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
+from raven.contrib.tornado import AsyncSentryClient, SentryMixin
 
-from tornado.web.RequestHandler import request, redirect, send_error
 from config import HOMEPAGE_URL, SHORT_URL_MAPPING
 
 
-class ShortUrlHandler(tornado.web.RequestHandler):
+class ShortUrlHandler(SentryMixin, tornado.web.RequestHandler):
     def get(self):
-        uri = request.uri.lstrip('/')
-        if len(uri) == 0:
-            redirect(HOMEPAGE_URL, permanent=True)
+        short = self.request.uri.lstrip('/')
+        if len(short) == 0:
+            self.redirect(HOMEPAGE_URL, permanent=True)
         else:
-            if uri in SHORT_URL_MAPPING:
-                redirect(SHORT_URL_MAPPING[uri], permanent=True)
+            if short in SHORT_URL_MAPPING:
+                self.redirect(SHORT_URL_MAPPING[short], permanent=True)
             else:
-                send_error(404)
+                self.send_error(404)
 
 
 application = tornado.web.Application([
     (r"/.*", ShortUrlHandler),
 ])
+SENTRY_DSN = os.environ.get('SENTRY_DSN')
+if SENTRY_DSN is not None:
+    application.sentry_client = AsyncSentryClient(SENTRY_DSN)
 
 
-if __name__ == "__main__":
+def main():
     http_server = tornado.httpserver.HTTPServer(application)
     http_port = int(os.environ.get("PORT", 5000))
     http_server.bind(http_port)
     http_server.start(0)
     tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
