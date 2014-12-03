8824548986a9fd2d39c229b144e7f474516e140b
Bo Zhu <github@bozhu.email>
2014-12-02 20:55:15 -0500

    save temp code and change computers

---
+++ config.py
@@ -0,0 +1,14 @@
+HOMEPAGE_URL = 'https://github.com/zhuzhuor/Unblock-Youku'
+
+SHORT_URL_MAPPING = {
+    'check': 'http://ipservice.163.com/isFromMainland',
+    'chrome': 'https://chrome.google.com/webstore/detail/unblock-youku/pdnfnkhpgegpcingjbfihlkjeighnddk/details',
+    'chrome-bug': 'https://github.com/zhuzhuor/Unblock-Youku/issues/45#issuecomment-10457179',
+    'coinbase': 'https://coinbase.com/?r=531f53c632f694d4b20000f9&utm_campaign=user-referral&src=referral-link',
+    'contributors': 'https://github.com/zhuzhuor/Unblock-Youku/contributors',
+    'crx': 'https://clients2.google.com/service/update2/crx?response=redirect&x=id%3Dpdnfnkhpgegpcingjbfihlkjeighnddk%26uc',
+    'custom': 'https://github.com/zhuzhuor/Unblock-Youku/wiki/Custom-backend-server-for-the-redirection-mode',
+    'dns': 'https://github.com/zhuzhuor/Unblock-Youku/issues/112',
+    'donate': 'https://coinbase.com/checkouts/1fd2ffdb109c17a9e7bacaa116d3dbc8',
+    'faq': 'https://github.com/zhuzhuor/Unblock-Youku/wiki/%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98%E7%9A%84%E8%A7%A3%E5%86%B3%E6%96%B9%E6%B3%95-FAQ',
+}
--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 
-# from tornado.web.RequestHandler import redirect
-from tornado.web import RedirectHandler
+from tornado.web.RequestHandler import request, redirect, send_error
+from config import HOMEPAGE_URL, SHORT_URL_MAPPING
 
 
 class ShortUrlHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        uri = request.uri.lstrip('/')
+        if len(uri) == 0:
+            redirect(HOMEPAGE_URL, permanent=True)
+        else:
+            if uri in SHORT_URL_MAPPING:
+                redirect(SHORT_URL_MAPPING[uri], permanent=True)
+            else:
+                send_error(404)
 
 
 application = tornado.web.Application([
-    (r"/", RedirectHandler, dict(url=HOMEPAGE_URL)),
     (r"/.*", ShortUrlHandler),
 ])
 
@@ -24,5 +30,5 @@ if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
     http_port = int(os.environ.get("PORT", 5000))
     http_server.bind(http_port)
-    http_server.start()
+    http_server.start(0)
     tornado.ioloop.IOLoop.instance().start()
