96a9f737b663c1ad57be09357fd007bc5b56ff16
zhuzhuor <zhuzhuor@users.noreply.github.com>
2016-05-16 22:25:01 -0700

    update the default link; change /feedback to the bbs; change the link for the PAC tutorial to the one on bbs

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 ANALYTICS_ID = 'UA-30726750-12'
 
-HOMEPAGE_URL = 'https://www.uku.im/index_zhs.html'
+HOMEPAGE_URL = 'https://www.uku.im/index.html'
 
 IGNORED_SHORT_URLS = frozenset([
     'favicon.ico', 'robots.txt', 'sitemap.xml', 'unit-test',
@@ -20,13 +20,13 @@ SHORT_URL_MAPPING = {
     'dns':          'https://github.com/Unblocker/Unblock-Youku/issues/618',
     'donate':       'https://coinbase.com/checkouts/1fd2ffdb109c17a9e7bacaa116d3dbc8',
     'faq':          'https://github.com/Unblocker/Unblock-Youku/wiki/%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98%E7%9A%84%E8%A7%A3%E5%86%B3%E6%96%B9%E6%B3%95-FAQ',
-    'feedback':     'https://groups.google.com/forum/#!forum/unblock-youku',
+    'feedback':     'https://bbs.uku.im/',
     'flash':        'https://github.com/Unblocker/Unblock-Youku/issues/209',
     'github':       'https://github.com/Unblocker/Unblock-Youku',
     'httpbin':      'http://httpbin.org/',  # for testing
     'malicious':    'https://github.com/Unblocker/Unblock-Youku/wiki/%E6%81%B6%E6%84%8F%E6%89%A9%E5%B1%95%E7%A8%8B%E5%BA%8F%E5%88%97%E8%A1%A8%E5%8F%8A%E7%94%84%E5%88%AB%E5%8A%9E%E6%B3%95',
     'modes':        'https://github.com/Unblocker/Unblock-Youku/wiki/%E8%BF%90%E8%A1%8C%E6%A8%A1%E5%BC%8F%E7%9A%84%E4%BB%8B%E7%BB%8D%E5%92%8C%E5%8C%BA%E5%88%AB',
-    'proxy':        'https://github.com/Unblocker/Unblock-Youku/wiki/%E4%BB%A3%E7%90%86%E6%9C%8D%E5%8A%A1%E5%99%A8%E8%AE%BE%E7%BD%AE%E7%A4%BA%E4%BE%8B',
+    'proxy':        'https://bbs.uku.im/t/27',
     'reviews':      'https://chrome.google.com/webstore/detail/unblock-youku/pdnfnkhpgegpcingjbfihlkjeighnddk/reviews',
     'screenshots':  'https://github.com/Unblocker/Unblock-Youku/wiki/%E5%B8%B8%E7%94%A8%E9%A1%B5%E9%9D%A2%E6%88%AA%E5%9B%BE%E7%A4%BA%E4%BE%8B',
     'share2facebook': 'https://www.facebook.com/sharer/sharer.php?u=https://chrome.google.com/webstore/detail/unblock-youku/pdnfnkhpgegpcingjbfihlkjeighnddk/',
